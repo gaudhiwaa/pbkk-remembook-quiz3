788c3d3d4b5fc7603ec99430b433f0a73d210786
Fathur45 <fathurahman450@gmail.com>
2022-05-12 20:11:32 +0700

    commit 1

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class RegisterController {
     @RequestMapping("/register")
     public String getRegister(Model model){
